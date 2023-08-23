97909884ea02d8b33d2270af4a5790e8afdc5856
Lolka servantov <Lolka@servant>
2023-08-23 11:22:52 +0300

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-static PROTOBUF_FILE: &str = "ProtobufDevice_0000E008.proto";
+static PROTOBUF_FILE: &str = "laser_setup.proto";
 static PROTOBUF_DIR: &str = "src/protobuf/proto";
 
 fn gen_protobuf() {
